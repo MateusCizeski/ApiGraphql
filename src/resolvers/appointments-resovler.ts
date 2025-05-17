6d375955c610dc0d103341ca9e8f9e466ccaf041
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-05-17 14:13:12 -0300

    finalizando api

--- src/resolvers/appointments-resovler.ts
+++ src/resolvers/appointments-resovler.ts
@@ -1,10 +1,35 @@
-import { Query, Resolver } from "type-graphql";
 import 'reflect-metadata';
+import { Arg, FieldResolver, Mutation, Query, Resolver, Root } from "type-graphql";
+import { CreateAppointmentInput } from "../dtos/inputs/create-appointment";
+import { Appointment } from '../dtos/models/appointment-model';
+import { Customer } from '../dtos/models/customer-model';
 
-@Resolver()
+@Resolver(() => Appointment)
 export class AppointmentsResolver {
-    @Query(() => String)
-    async helloWorld() {
-        return 'hello world'
+    @Query(() => [Appointment])
+    async appointments() {
+       return [{
+        startsAt: new Date(),
+        endsAt: new Date()
+       }];
+    }
+
+    @Mutation(() => Appointment)
+    async createAppointment(@Arg('data') data: CreateAppointmentInput) {
+        const appointment = {
+            startsAt: data.startsAt,
+            endsAt: data.endsAt
+        };
+        
+        return appointment;
+    }
+
+    @FieldResolver(() => Customer)
+    async customer(@Root() appointment: Appointment) {
+        console.log(appointment);
+
+        return {
+            name: "teste"
+        }
     }
 }
